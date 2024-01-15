bea23bf39a9a237f5451c7d5c5b8f3f35ece2080
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-15 16:32:47 -0700

    :recycle: Comment out code for redirecting to sign in.
    It blocks access to the /status route, and there is some kind of race condition as well.

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,25 +5,25 @@
 </template>
 
 <script lang="ts" setup>
-import { useAuth0 } from "@auth0/auth0-vue"
-import { watch } from "vue"
-import { useRouter } from "vue-router"
+// import { useAuth0 } from "@auth0/auth0-vue"
+// import { watch } from "vue"
+// import { useRouter } from "vue-router"
 
-const router = useRouter()
-const { isLoading, isAuthenticated } = useAuth0()
+// const router = useRouter()
+// const { isLoading, isAuthenticated } = useAuth0()
 
 // TODO: this is hacked-together, it should reworked before final usage
-watch(
-  () => [isLoading.value, isAuthenticated.value],
-  ([newIsLoading, newIsAuthenticated], _) => {
-    if (newIsLoading === false && newIsAuthenticated === true) {
-      router.push("/dashboard")
-    } else if (!newIsLoading && !isAuthenticated.value) {
-      router.push("/sign-in")
-    }
-  },
-  {
-    immediate: true,
-  }
-)
+// watch(
+//   () => [isLoading.value, isAuthenticated.value],
+//   ([newIsLoading, newIsAuthenticated], _) => {
+//     if (newIsLoading === false && newIsAuthenticated === true) {
+//       router.push("/dashboard")
+//     } else if (!newIsLoading && !isAuthenticated.value) {
+//       router.push("/sign-in")
+//     }
+//   },
+//   {
+//     immediate: true,
+//   }
+// )
 </script>
