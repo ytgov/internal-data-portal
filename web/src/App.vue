1e02f9a9d172371daefdc8891a5578a2ec2229c8
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-16 15:07:53 -0700

    :sparkles: Support navigation to unauthenticated routes.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
+    <router-view v-if="isUnauthenticatedRoute"></router-view>
     <!--
-      NOTE: current user will always be defined when the router view loads.
-      I'll have to test how this affect unauthenticated routes ...
+      NOTE: current user will always be defined when the authenticated router view loads.
     -->
-    <router-view v-if="isReady" />
+    <router-view v-else-if="isReady" />
     <PageLoader v-else />
   </v-app>
 </template>
@@ -12,9 +12,13 @@
 <script lang="ts" setup>
 import { computed, watch } from "vue"
 import { useAuth0 } from "@auth0/auth0-vue"
+import { useRoute } from "vue-router"
 
 import useCurrentUser from "@/use/use-current-user"
 import PageLoader from "@/components/PageLoader.vue"
+
+const route = useRoute()
+const isUnauthenticatedRoute = computed(() => route.meta.requiresAuth === false)
 
 const { isLoading: isLoadingAuth0, isAuthenticated } = useAuth0()
 const isReadyAuth0 = computed(() => !isLoadingAuth0.value && isAuthenticated.value)
@@ -25,11 +29,12 @@ const isReady = computed(() => isReadyAuth0.value && isReadyCurrentUser.value)
 watch(
   () => isReadyAuth0.value,
   async (newIsReadyAuth0) => {
+    // Don't bother attempting to load current user for unathenticated routes
+    if (isUnauthenticatedRoute.value) return
+
     if (newIsReadyAuth0 === true) {
       try {
         await ensure()
-        console.log("isReadyCurrentUser.value:", isReadyCurrentUser.value)
-        console.log("isReady.value:", isReady.value)
       } catch (error) {
         console.log("Failed to ensure current user:", error)
         // Toast/snack Please contact support ...
