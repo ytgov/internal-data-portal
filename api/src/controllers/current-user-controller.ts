e7354372e08cf30ecf416a43317e634783d1769e
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-16 15:19:53 -0700

    :pencil: Add note about alternative serial interface.

--- api/src/controllers/current-user-controller.ts
+++ api/src/controllers/current-user-controller.ts
@@ -4,6 +4,7 @@ import { UserSerializers } from "@/serializers"
 
 export class CurrentUserController extends BaseController {
   async show() {
+    // TODO: consider changing interface to Users.AsDetailedSerializer.perform()?
     const serializedUser = UserSerializers.asDetailed(this.currentUser)
     return this.response.status(200).json({ user: serializedUser })
   }
