17de02b86ad338680187dde9634dd863e9c329e7
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-16 09:26:55 -0700

    :construction: Add logout button to dashboard.
    Mostly just proof of concept.

--- web/src/pages/DashboardPage.vue
+++ web/src/pages/DashboardPage.vue
@@ -1,13 +1,35 @@
-<template>Dashboard</template>
+<template>
+  Dashboard
+
+  <pre>
+    {{ user }}
+  </pre>
+
+  <v-btn
+    dark
+    color="primary"
+    @click="logoutWrapper"
+    >Log Out</v-btn
+  >
+</template>
 
 <script lang="ts" setup>
 import { useAuth0 } from "@auth0/auth0-vue"
 import { onMounted } from "vue"
 
-const { isLoading, isAuthenticated } = useAuth0()
+const { isLoading, isAuthenticated, user, logout } = useAuth0()
 
 onMounted(() => {
   console.log("isLoading.value:", isLoading.value)
   console.log("isAuthenticated.value:", isAuthenticated.value)
 })
+
+async function logoutWrapper() {
+  await logout({
+    logoutParams: {
+      // I would prefer to redirect to /sign-in here, but that doesn't seem to work?
+      returnTo: window.location.origin,
+    },
+  })
+}
 </script>
