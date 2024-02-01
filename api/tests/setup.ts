c19d94468ce948ccf0f0c1dcec562307e8f88d94
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-01 09:50:16 -0700

    :recycle: Simplify test setup as all tables are in Sequelize.

--- api/tests/setup.ts
+++ api/tests/setup.ts
@@ -13,44 +13,11 @@ jest object and expect accessible in the modules. For example, you can add extra
 from jest-extended library or call setup and teardown hooks.
 */
 
-import { QueryTypes } from "sequelize"
-
 import db from "@/models"
 
-async function getTableNames() {
-  const query = `
-    SELECT table_name as "tableName"
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-    AND table_type = 'BASE TABLE'
-    AND table_name != 'SequelizeMeta'
-    AND table_name != 'knex_migrations'
-    AND table_name != 'knex_migrations_lock';
-  `
-
-  try {
-    const result = await db.query<{ tableName: string }>(query, { type: QueryTypes.SELECT })
-    const tableNames = result.map((row) => row.tableName)
-    return tableNames
-  } catch (error) {
-    console.error("Error fetching table names:", error)
-    throw error
-  }
-}
-
 async function cleanDatabase() {
-  const tableNames = await getTableNames()
-  const quotedTableNames = tableNames.map((name) => `"${name}"`)
-  const truncateQuery = `
-    TRUNCATE TABLE
-      ${quotedTableNames.join(",\n      ")}
-    RESTART IDENTITY
-    CASCADE;
-  `
   try {
-    // TODO: once all tables are in Sequelize models, use this instead:
-    // await db.truncate({ cascade: true, restartIdentity: true })
-    await db.query(truncateQuery, { raw: true })
+    await db.truncate({ cascade: true, restartIdentity: true })
     return true
   } catch (error) {
     console.error(error)
