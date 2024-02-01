dbbd633c430a39017dc92f669d48e61f85c9c5e6
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-01 16:43:23 -0700

    :beetle: Fix database cleanup function.
    Apparently MSSQL just doesn't support truncate if you have foreign keys, no matter whether you turn of all the constraints or not.

--- api/tests/setup.ts
+++ api/tests/setup.ts
@@ -15,9 +15,36 @@ from jest-extended library or call setup and teardown hooks.
 
 import db from "@/models"
 
+/*
+  e.g.
+  EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL';
+  DELETE FROM table1 WHERE 1=1;
+  DELETE FROM table2 WHERE 1=1;
+  ...
+  DBCC CHECKIDENT ('table1', RESEED, 0);
+  DBCC CHECKIDENT ('table2', RESEED, 0);
+  ...
+  EXEC sp_msforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL';
+*/
+const tableNames = Object.values(db.models).map((model) => model.tableName)
+const disableAllConstraintsQuery = "EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL';"
+const deleteAllInAllTablesQuery = tableNames
+  .map((tableName) => `DELETE FROM ${tableName} WHERE 1=1;`)
+  .join("\n")
+const resetIdentityColumnsQuery = tableNames
+  .map((tableName) => `DBCC CHECKIDENT ('${tableName}', RESEED, 0);`)
+  .join("\n")
+const enableAllConstraintsQuery = "EXEC sp_msforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL';"
+const cleanupQuery = [
+  disableAllConstraintsQuery,
+  deleteAllInAllTablesQuery,
+  resetIdentityColumnsQuery,
+  enableAllConstraintsQuery,
+].join("\n")
+
 async function cleanDatabase() {
   try {
-    await db.truncate({ cascade: true, restartIdentity: true })
+    await db.query(cleanupQuery, { raw: true }).catch(console.error)
     return true
   } catch (error) {
     console.error(error)
