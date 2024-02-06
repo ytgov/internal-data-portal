2dfd41c58459fa1c676fd319c01014fb2abcfa63
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-06 12:02:14 -0700

    :beetle: Fix attribute name in apply random access grants controller.

--- api/src/controllers/qa-scenarios/apply-random-access-grants-controller.ts
+++ api/src/controllers/qa-scenarios/apply-random-access-grants-controller.ts
@@ -32,7 +32,7 @@ export class ApplyRandomAccessGrantsController extends BaseController {
 
       const randomAccessGrantAttributes: CreationAttributes<AccessGrant>[] = randomGrantLevels.map((randomGrantLevel) => ({
         datasetId: dataset.id,
-        ownerId: dataset.ownerId,
+        creatorId: dataset.ownerId,
         // TODO: requestorId: faker.helpers.arrayElement(users - dataset.owner).id,
         grantLevel: randomGrantLevel,
         accessType: faker.helpers.arrayElement(Object.values(AccessTypes)),
