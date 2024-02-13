dd4a051e6da1c1281a70d556d1b5befc94467f42
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-13 14:08:33 -0700

    :construction: Build out dataset fields page stubs and routing.

---
+++ web/src/pages/DatasetFieldsManagePage.vue
@@ -0,0 +1,47 @@
+<template>
+  TODO: build out dataset fields manage page.
+
+  <pre>{{ dataset }}</pre>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue"
+
+import { useBreadcrumbs } from "@/use/use-breadcrumbs"
+import { useDataset } from "@/use/use-dataset"
+
+const props = defineProps({
+  slug: {
+    type: String,
+    required: true,
+  },
+})
+
+const { setBreadcrumbs } = useBreadcrumbs()
+
+setBreadcrumbs([
+  {
+    title: "Datasets",
+    to: {
+      name: "DatasetsPage",
+    },
+  },
+  {
+    title: "Fields",
+    to: {
+      name: "DatasetFieldsReadPage",
+      params: { slug: props.slug },
+    },
+  },
+  {
+    title: "Manage",
+    to: {
+      name: "DatasetFieldsManagePage",
+      params: { slug: props.slug },
+    },
+  },
+])
+
+const { slug } = toRefs(props)
+const { dataset } = useDataset(slug)
+</script>
