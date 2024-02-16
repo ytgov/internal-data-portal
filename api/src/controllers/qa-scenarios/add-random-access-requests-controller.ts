ed4eb9680181edd3e73ea4a7e4c348f8d9fc90b9
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-15 18:10:18 -0700

    :sparkles: Fix random access request QA helper so it handle single requestor per dataset.
    Its a bit messy since it will infinitely create new users ...

--- api/src/controllers/qa-scenarios/add-random-access-requests-controller.ts
+++ api/src/controllers/qa-scenarios/add-random-access-requests-controller.ts
@@ -2,10 +2,12 @@ import { faker } from "@faker-js/faker"
 import { CreationAttributes } from "sequelize"
 import { times } from "lodash"
 
-import { AccessGrant, AccessRequest, User } from "@/models"
+import { AccessGrant, AccessRequest, Role, User, UserGroup, UserGroupMembership } from "@/models"
 import { AccessTypes } from "@/models/access-grant"
 
 import BaseController from "@/controllers/base-controller"
+import { RoleTypes } from "@/models/role"
+import { UserGroupTypes } from "@/models/user-groups"
 
 export class AddRandomAccessRequestsController extends BaseController {
   async create() {
@@ -25,13 +27,19 @@ export class AddRandomAccessRequestsController extends BaseController {
   private async addRandomAccessRequestsToDatasets() {
     const accessGrants = await AccessGrant.findAll()
     const users = await User.findAll()
+    const departments = await UserGroup.findAll({
+      where: {
+        type: UserGroupTypes.DEPARTMENT,
+      },
+    })
+
+    const datasetIdToRequestorIdMap = new Map<number, number>()
 
     const promises = accessGrants.map(async (accessGrant) => {
       const numberOfAccessRequestsPerGrant = faker.number.int({ min: 0, max: 3 })
 
-      const randomAccessRequestAttributes: CreationAttributes<AccessRequest>[] = times(
-        numberOfAccessRequestsPerGrant,
-        () => {
+      const randomAccessRequestAttributes: CreationAttributes<AccessRequest>[] = await Promise.all(
+        times(numberOfAccessRequestsPerGrant, async () => {
           let projectAttributes = {}
           if (accessGrant.isProjectDescriptionRequired) {
             projectAttributes = {
@@ -48,21 +56,53 @@ export class AddRandomAccessRequestsController extends BaseController {
             }
           }
 
+          let requestor = faker.helpers.arrayElement(users)
+          if (datasetIdToRequestorIdMap.has(accessGrant.datasetId)) {
+            requestor = await this.createFakeUser(departments)
+          }
+
+          datasetIdToRequestorIdMap.set(accessGrant.datasetId, requestor.id)
+
           return {
             datasetId: accessGrant.datasetId,
             accessGrantId: accessGrant.id,
-            requestorId: faker.helpers.arrayElement(users).id,
+            requestorId: requestor.id,
             accessCode: faker.string.alphanumeric(10),
             ...projectAttributes,
             ...approvalAttributes,
           }
-        }
+        })
       )
 
       return AccessRequest.bulkCreate(randomAccessRequestAttributes)
     })
 
     return Promise.all(promises)
+  }
+
+  private async createFakeUser(departments: UserGroup[]): Promise<User> {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+
+    const user = await User.create({
+      auth0Subject: `auth0|${faker.string.uuid()}`,
+      email: faker.internet.email({ firstName, lastName }),
+      firstName,
+      lastName,
+      position: faker.person.jobTitle(),
+      lastEmployeeDirectorySyncAt: faker.date.recent(),
+    })
+
+    await Role.create({
+      userId: user.id,
+      role: faker.helpers.enumValue(RoleTypes),
+    })
+
+    await UserGroupMembership.create({
+      userId: user.id,
+      departmentId: faker.helpers.arrayElement(departments).id,
+    })
+    return user
   }
 }
 
