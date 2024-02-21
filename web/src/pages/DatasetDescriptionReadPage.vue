c4ddbf0b17f679fa37ff5504c6e53a54017ce471
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-21 11:08:45 -0700

    :sparkles: Add owner card to dataset description read page.

--- web/src/pages/DatasetDescriptionReadPage.vue
+++ web/src/pages/DatasetDescriptionReadPage.vue
@@ -1,11 +1,21 @@
 <template>
   <DataDescriptionCard :slug="slug" />
+
+  <v-row class="mt-4">
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <OwnerCard :slug="slug" />
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import { useBreadcrumbs } from "@/use/use-breadcrumbs"
 
 import DataDescriptionCard from "@/components/datasets/DataDescriptionCard.vue"
+import OwnerCard from "@/components/datasets/OwnerCard.vue"
 
 const props = defineProps({
   slug: {
