09d354a5ddb1d35c819a6a461f59bf9c5298aa5d
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-21 11:56:12 -0700

    :construction: Add QA helper to cycle through all users roles.
    This should not be run in production.

--- api/src/router.ts
+++ api/src/router.ts
@@ -116,6 +116,9 @@ router
 router
   .route("/api/qa-scenarios/add-random-access-requests")
   .post(QaScenarios.AddRandomAccessRequestsController.create)
+router
+  .route("/api/qa-scenarios/cycle-user-role-type")
+  .post(QaScenarios.CycleUserRoleTypeController.create)
 
 // if no other routes match, return a 404
 router.use("/api", (req: Request, res: Response) => {
--- web/src/components/qa-scenarios/QaScenariosCard.vue
+++ web/src/components/qa-scenarios/QaScenariosCard.vue
@@ -27,10 +27,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue"
+import { computed } from "vue"
+import { isNil } from "lodash"
 
 import http from "@/api/http-client"
 import useSnack from "@/use/use-snack"
+import useCurrentUser, { RoleTypes } from "@/use/use-current-user"
 
 type Scenario = {
   url: string
@@ -39,8 +41,29 @@ type Scenario = {
 }
 
 const snack = useSnack()
+const { currentUser, fetch: refreshCurrentUser } = useCurrentUser()
 
-const scenarios = ref<Scenario[]>([
+// Keep in sync with api/src/controllers/qa-scenarios/cycle-user-role-type.ts
+const ORDERED_ROLE_TYPES = [
+  RoleTypes.USER,
+  RoleTypes.DATA_OWNER,
+  RoleTypes.BUSINESS_ANALYST,
+  RoleTypes.SYSTEM_ADMIN,
+]
+
+const nextRoleType = computed<RoleTypes | null>(() => {
+  if (isNil(currentUser.value)) return null
+
+  const { roleTypes } = currentUser.value
+  const currentRoleType = roleTypes[0]
+  if (isNil(currentRoleType)) return null
+
+  const currentIndex = ORDERED_ROLE_TYPES.indexOf(currentRoleType)
+  const nextIndex = (currentIndex + 1) % 4
+  return ORDERED_ROLE_TYPES[nextIndex]
+})
+
+const scenarios = computed<Scenario[]>(() => [
   {
     url: "/api/qa-scenarios/link-random-tags",
     label: "Link Random Tags",
@@ -53,12 +76,17 @@ const scenarios = ref<Scenario[]>([
     url: "/api/qa-scenarios/add-random-access-requests",
     label: "Add Random Subscriptions (Access Requests)",
   },
+  {
+    url: "/api/qa-scenarios/cycle-user-role-type",
+    label: `Cycle User Type To ${nextRoleType.value || "..."}`,
+  },
 ])
 
 async function triggerScenario(scenario: Scenario) {
   scenario.isLoading = true
   try {
     const { data } = await http.post(scenario.url)
+    await refreshCurrentUser()
     snack.notify(data.message, { color: "success" })
   } catch (error: unknown) {
     if (error instanceof Error) {
