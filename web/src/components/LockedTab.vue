eaccf81c7d2a34ca3afc0020fd5424b0f18a57dc
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-21 16:39:29 -0700

    :construction: Begin adding locked tab helper component.

---
+++ web/src/components/LockedTab.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-tab
+    disabled
+    append-icon="mdi-lock"
+    >
+    <template
+      v-for="(_, slotName) of $slots"
+      #[slotName]="slotProps"
+      ><slot
+        :name="slotName"
+        v-bind="slotProps || {}"
+      ></slot
+    ></template>
+  </v-tab>
+</template>
+
+<script lang="ts" setup></script>
