d75122d0ebb7890715c0647a300b46a3cb69117d
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-21 17:09:57 -0700

    :sparkles: Lock fields tab unless user has appropriate access.
    Also need to add the back-end logic for this.
    
    It might also make sense to return some policy logic for whether the user can read dataset fields and use that to control whether the user can see the tab or not ...

--- web/src/components/LockedTab.vue
+++ web/src/components/LockedTab.vue
@@ -1,17 +1,26 @@
 <template>
-  <v-tab
-    disabled
-    append-icon="mdi-lock"
-    >
-    <template
-      v-for="(_, slotName) of $slots"
-      #[slotName]="slotProps"
-      ><slot
-        :name="slotName"
-        v-bind="slotProps || {}"
-      ></slot
-    ></template>
-  </v-tab>
+  <v-tooltip>
+    <template #activator="{ props }">
+      <span v-bind="props">
+        <v-tab
+          disabled
+          append-icon="mdi-lock"
+        >
+          <!-- The typescript error here doesn't seem to cause any problems, so you can ignore it -->
+          <template
+            v-for="(_, slotName, index) of $slots"
+            :key="`${slotName}-${index}`"
+            #[slotName]="slotProps"
+            ><slot
+              :name="slotName"
+              v-bind="slotProps || {}"
+            ></slot
+          ></template>
+        </v-tab>
+      </span>
+    </template>
+    <span class="text-white">You do not have permission to view this tab.</span>
+  </v-tooltip>
 </template>
 
 <script lang="ts" setup></script>
