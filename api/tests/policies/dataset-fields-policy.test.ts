dc31e6f352f0e28fa5c2b6d6a946eb7193a39ad6
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-22 14:29:59 -0700

    :x: Add failing test for restricting fields for user type role.

--- api/tests/policies/dataset-fields-policy.test.ts
+++ api/tests/policies/dataset-fields-policy.test.ts
@@ -1,8 +1,18 @@
 import { DatasetField } from "@/models"
 import { RoleTypes } from "@/models/role"
+import { UserGroupTypes } from "@/models/user-groups"
 import { DatasetFieldsPolicy } from "@/policies"
 
-import { datasetFactory, datasetFieldFactory, roleFactory, userFactory } from "@/factories"
+import {
+  accessGrantFactory,
+  datasetFactory,
+  datasetFieldFactory,
+  roleFactory,
+  userFactory,
+  userGroupFactory,
+  userGroupMembershipFactory,
+} from "@/factories"
+import { AccessTypes, GrantLevels } from "@/models/access-grant"
 
 describe("api/src/policies/dataset-fields-policy.ts", () => {
   describe("DatasetFieldsPolicy", () => {
@@ -39,6 +49,49 @@ describe("api/src/policies/dataset-fields-policy.ts", () => {
           ])
         }
       )
+
+      test("when user has role type user, and field belongs to dataset without accessible grants, returns nothing", async () => {
+        // Arrange
+        const department = await userGroupFactory.create({ type: UserGroupTypes.DEPARTMENT })
+        const requestingUserGroupMembership = userGroupMembershipFactory.build({
+          departmentId: department.id,
+        })
+        const datasetOwnerGroupMembership = userGroupMembershipFactory.build({
+          departmentId: department.id,
+        })
+        const role = roleFactory.build({ role: RoleTypes.USER })
+        const requestingUser = await userFactory
+          .transient({
+            include: ["groupMembership"],
+          })
+          .associations({
+            roles: [role],
+            groupMembership: requestingUserGroupMembership,
+          })
+          .create()
+
+        const datasetOwner = await userFactory
+          .associations({
+            groupMembership: datasetOwnerGroupMembership,
+          })
+          .create()
+
+        const dataset = await datasetFactory.create({
+          creatorId: datasetOwner.id,
+          ownerId: datasetOwner.id,
+        })
+        await datasetFieldFactory.create({
+          datasetId: dataset.id,
+        })
+        const scopedQuery = DatasetFieldsPolicy.applyScope(DatasetField, requestingUser)
+
+        // Act
+        const result = await scopedQuery.findAll()
+
+        // Assert
+        expect(DatasetField.count()).resolves.toBe(1)
+        expect(result).toHaveLength(0)
+      })
     })
   })
 })
