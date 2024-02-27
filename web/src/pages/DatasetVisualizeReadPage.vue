2c3c266ff8ee4bf792efc12218f589434b1ff33d
Marlen Brunner <klondikemarlen@gmail.com>
2024-02-27 14:32:26 -0700

    :construction: Add edit button to dataset visualize read page
    If the user had update permissions.

--- web/src/pages/DatasetVisualizeReadPage.vue
+++ web/src/pages/DatasetVisualizeReadPage.vue
@@ -1,11 +1,30 @@
 <template>
   TODO: build DatasetVisualizeReadPage
 
-  {{ dataset }}
+  <v-progress-circular
+      v-if="isNil(dataset) || isNil(policy)"
+      indeterminate
+      color="primary"
+      size="36"
+    />
+    <v-btn
+      v-else-if="policy.update"
+      color="primary"
+      :to="{
+        name: 'DatasetVisualizeManagePage',
+        params: { slug },
+      }"
+    >
+      Edit
+    </v-btn>
+    <template v-else>
+      <!-- user does not have edit rights for dataset fields -->
+    </template>
 </template>
 
 <script setup lang="ts">
 import { toRefs } from "vue"
+import { isNil } from "lodash"
 
 import { useBreadcrumbs } from "@/use/use-breadcrumbs"
 import { useDataset } from "@/use/use-dataset"
@@ -18,7 +37,7 @@ const props = defineProps({
 })
 
 const { slug } = toRefs(props)
-const { dataset } = useDataset(slug)
+const { dataset, policy } = useDataset(slug)
 
 const { setBreadcrumbs } = useBreadcrumbs()
 
