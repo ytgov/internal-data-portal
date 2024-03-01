573a64e5296c32190b53481fe0883f2bab435ed2
Marlen Brunner <klondikemarlen@gmail.com>
2024-03-01 11:22:37 -0700

    :fire: Remove status from dataset model.
    Its now part of the dataset integration model.

--- api/src/serializers/datasets/table-serializer.ts
+++ api/src/serializers/datasets/table-serializer.ts
@@ -28,7 +28,6 @@ export class TableSerializer extends BaseSerializer<Dataset> {
         "slug",
         "name",
         "description",
-        "status",
         "publishedAt",
         "deactivatedAt",
         "createdAt",
