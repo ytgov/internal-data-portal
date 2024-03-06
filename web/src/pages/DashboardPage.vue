85539f7aa769e319151da69f29074b414867e3f2
Marlen Brunner <klondikemarlen@gmail.com>
2024-03-06 12:03:17 -0700

    :construction: Build boilerplate for admin dashboard.

--- web/src/pages/DashboardPage.vue
+++ web/src/pages/DashboardPage.vue
@@ -5,21 +5,60 @@
       <div class="d-flex flex-column flex-md-row mt-4 mt-md-0">
         <v-btn
           color="primary"
-          :to="{ name: 'DatasetNewPage' }"
-          >Create Dataset</v-btn
-        >
-        <v-btn
-          class="ml-md-4 mt-4 mt-md-0"
-          color="primary"
           variant="outlined"
           :to="{ name: 'DatasetsPage' }"
         >
           View Datasets
         </v-btn>
+        <v-btn
+          class="ml-md-4 mt-4 mt-md-0"
+          color="primary"
+          :to="{ name: 'DatasetNewPage' }"
+          >Create Dataset</v-btn
+        >
       </div>
     </h2>
-
-    <v-row class="mt-10">
+    <v-row class="mt-6">
+      <v-col
+        cols="12"
+        md="6"
+      >
+        TODO: DatasetSearchCard
+        <v-skeleton-loader
+          type="card"
+          boilerplate
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        TODO: MySubscriptionsCard
+        <v-skeleton-loader
+          type="card"
+          boilerplate
+        />
+        TODO: MyDataCard
+        <v-skeleton-loader
+          type="card"
+          boilerplate
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        TODO: MyRequestsCard
+        <v-skeleton-loader
+          type="card"
+          boilerplate
+        />
+      </v-col>
+    </v-row>
+    <v-divider
+      thickness="1"
+      class="my-8 border-opacity-100"
+    />
+    <v-row>
       <v-col
         cols="12"
         md="6"
