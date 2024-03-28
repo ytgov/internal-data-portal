1a8bfd361f1b3ee0a59cff585e9ed2b6bea3f48f
Marlen Brunner <klondikemarlen@gmail.com>
2024-03-28 15:40:51 -0700

    :recycle: Switch to using enhanced user group picker in new dataset page.
    Why? So that the picker don't go out of range until there are more than 1000 entries.
    Hopefully before that point I'll have time to build out a search based solution.

--- web/src/components/datasets/NewDatasetForm.vue
+++ web/src/components/datasets/NewDatasetForm.vue
@@ -129,17 +129,14 @@
         cols="12"
         md="6"
       >
-        <v-autocomplete
-          :model-value="datasetStewardship.departmentId"
-          :items="departments"
-          :disabled="datasetStewardship.ownerId === undefined || departments.length === 0"
-          :loading="isLoadingDepartments"
-          :rules="[required]"
-          item-value="id"
-          item-title="name"
+        <UserGroupAutocomplete
           label="Department *"
+          :model-value="datasetStewardship.departmentId"
+          :type="UserGroupTypes.DEPARTMENT"
+          :parent-id="null"
+          :disabled="datasetStewardship.ownerId === undefined"
+          :rules="[required]"
           variant="outlined"
-          auto-select-first
           clearable
           required
           @update:model-value="updateDepartment($event as unknown as number | null)"
@@ -149,16 +146,13 @@
         cols="12"
         md="6"
       >
-        <v-autocomplete
-          :model-value="datasetStewardship.divisionId"
-          :items="divisions"
-          :loading="isLoadingDivisions"
-          :disabled="isNil(datasetStewardship.departmentId) || divisions.length === 0"
-          item-value="id"
-          item-title="name"
+        <UserGroupAutocomplete
           label="Division"
+          :model-value="datasetStewardship.divisionId"
+          :type="UserGroupTypes.DIVISION"
+          :parent-id="datasetStewardship.departmentId"
+          :disabled="isNil(datasetStewardship.departmentId)"
           variant="outlined"
-          auto-select-first
           clearable
           @update:model-value="updateDivision($event as unknown as number | null)"
         />
@@ -169,16 +163,13 @@
         cols="12"
         md="6"
       >
-        <v-autocomplete
-          :model-value="datasetStewardship.branchId"
-          :items="branches"
-          :loading="isLoadingBranches"
-          :disabled="isNil(datasetStewardship.divisionId) || branches.length === 0"
-          item-value="id"
-          item-title="name"
+        <UserGroupAutocomplete
           label="Branch"
+          :model-value="datasetStewardship.branchId"
+          :type="UserGroupTypes.BRANCH"
+          :parent-id="datasetStewardship.divisionId"
+          :disabled="isNil(datasetStewardship.divisionId)"
           variant="outlined"
-          auto-select-first
           clearable
           @update:model-value="updateBranch($event as unknown as number | null)"
         />
@@ -187,16 +178,13 @@
         cols="12"
         md="6"
       >
-        <v-autocomplete
-          :model-value="datasetStewardship.unitId"
-          :items="units"
-          :loading="isLoadingUnits"
-          :disabled="isNil(datasetStewardship.branchId) || units.length === 0"
-          item-value="id"
-          item-title="name"
+        <UserGroupAutocomplete
           label="Unit"
+          :model-value="datasetStewardship.unitId"
+          :type="UserGroupTypes.UNIT"
+          :parent-id="datasetStewardship.branchId"
+          :disabled="isNil(datasetStewardship.branchId)"
           variant="outlined"
-          auto-select-first
           clearable
           @update:model-value="updateUnit($event as unknown as number | null)"
         />
@@ -231,23 +219,24 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from "vue"
+import { ref } from "vue"
 import { useRouter } from "vue-router"
 import { isNil } from "lodash"
 
 import { type VForm } from "vuetify/lib/components/index.mjs"
 
 import { type DatasetStewardship } from "@/api/dataset-stewardships-api"
-import { UserGroupTypes } from "@/api/user-groups-api"
 import datasetsApi, { type Dataset } from "@/api/datasets-api"
 import usersApi from "@/api/users-api"
 
 import useSnack from "@/use/use-snack"
-import useUserGroups from "@/use/use-user-groups"
 
 import { required } from "@/utils/validators"
 
 import UserSearchableAutocomplete from "@/components/users/UserSearchableAutocomplete.vue"
+import UserGroupAutocomplete, {
+  UserGroupTypes,
+} from "@/components/user-groups/UserGroupAutocomplete.vue"
 
 const router = useRouter()
 const snack = useSnack()
@@ -257,52 +246,6 @@ const isLoading = ref(false)
 const dataset = ref<Partial<Dataset>>({})
 
 const datasetStewardship = ref<Partial<DatasetStewardship>>({})
-
-const departmentId = computed(() => datasetStewardship.value.departmentId)
-const divisionId = computed(() => datasetStewardship.value.divisionId)
-const branchId = computed(() => datasetStewardship.value.branchId)
-const departmentsQuery = computed(() => ({
-  where: {
-    type: UserGroupTypes.DEPARTMENT,
-  },
-}))
-const divisionsQuery = computed(() => ({
-  where: {
-    type: UserGroupTypes.DIVISION,
-    parentId: departmentId.value,
-  },
-}))
-const branchesQuery = computed(() => ({
-  where: {
-    type: UserGroupTypes.BRANCH,
-    parentId: divisionId.value,
-  },
-}))
-const unitsQuery = computed(() => ({
-  where: {
-    type: UserGroupTypes.UNIT,
-    parentId: branchId.value,
-  },
-}))
-const { userGroups: departments, isLoading: isLoadingDepartments } = useUserGroups(
-  departmentsQuery,
-  { immediate: true }
-)
-const {
-  userGroups: divisions,
-  isLoading: isLoadingDivisions,
-  fetch: fetchDivisions,
-} = useUserGroups(divisionsQuery, { immediate: false })
-const {
-  userGroups: branches,
-  isLoading: isLoadingBranches,
-  fetch: fetchBranches,
-} = useUserGroups(branchesQuery, { immediate: false })
-const {
-  userGroups: units,
-  isLoading: isLoadingUnits,
-  fetch: fetchUnits,
-} = useUserGroups(unitsQuery, { immediate: false })
 
 async function updateOwner(newOwnerId: number | null) {
   if (isNil(newOwnerId)) {
@@ -382,10 +325,6 @@ async function updateDivision(newDivisionId: number | null) {
     return
   }
 
-  if (divisions.value.length === 0) {
-    await fetchDivisions()
-  }
-
   datasetStewardship.value.divisionId = newDivisionId
   clearBranch()
 }
@@ -396,10 +335,6 @@ async function updateBranch(newBranchId: number | null) {
     return
   }
 
-  if (branches.value.length === 0) {
-    await fetchBranches()
-  }
-
   datasetStewardship.value.branchId = newBranchId
   clearUnit()
 }
@@ -408,10 +343,6 @@ async function updateUnit(newUnitId: number | null) {
   if (isNil(newUnitId)) {
     clearUnit()
     return
-  }
-
-  if (units.value.length === 0) {
-    await fetchUnits()
   }
 
   datasetStewardship.value.unitId = newUnitId
