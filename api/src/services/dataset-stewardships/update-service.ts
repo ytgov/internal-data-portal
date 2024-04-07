fe8c43fec1ba2c0688d2aa15733ad54c08922679
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-07 15:21:02 -0700

    :sparkles: Make it possible to update a dataset owner after dataset creation.
    I probably shouldn't have made the dataset stewardship have a separate ownerId field than the dataset.

--- api/src/services/dataset-stewardships/update-service.ts
+++ api/src/services/dataset-stewardships/update-service.ts
@@ -1,4 +1,4 @@
-import { DatasetStewardship, User } from "@/models"
+import db, { Dataset, DatasetStewardship, User } from "@/models"
 
 import BaseService from "@/services/base-service"
 
@@ -14,11 +14,31 @@ export class UpdateService extends BaseService {
   }
 
   async perform(): Promise<DatasetStewardship> {
-    await this.datasetStewardship.update(this.attributes)
+    return db.transaction(async () => {
+      this.datasetStewardship.set(this.attributes)
 
-    // TODO: log user who performed update?
+      await this.propagateOwnerChangeToDataset(this.datasetStewardship)
 
-    return this.datasetStewardship
+      // TODO: log user who performed update?
+
+      return this.datasetStewardship.save()
+    })
+  }
+
+  /**
+   * TODO: figure out how to make this code unnecessary.
+   * I probably shouldn't have made the dataset stewardship
+   * have a separate ownerId field than the dataset.
+   */
+  private async propagateOwnerChangeToDataset(
+    datasetStewardship: DatasetStewardship
+  ): Promise<void> {
+    if (datasetStewardship.changed("ownerId")) {
+      await Dataset.update(
+        { ownerId: datasetStewardship.ownerId },
+        { where: { id: datasetStewardship.datasetId } }
+      )
+    }
   }
 }
 
