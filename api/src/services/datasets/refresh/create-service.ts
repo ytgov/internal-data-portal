96cbe9f7ddf9ed54487ba011136744a99a680a77
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-09 10:10:34 -0700

    :pencil: Fix some wording in a comment.

--- api/src/services/datasets/refresh/create-service.ts
+++ api/src/services/datasets/refresh/create-service.ts
@@ -3,8 +3,8 @@ import db, { DatasetIntegration, User } from "@/models"
 import BaseService from "@/services/base-service"
 
 /**
- * Note that will this is the same as the dataset-integrations/update-service.ts currently,
- * its purpose is not quite the same, so keeping it separte for now despite the duplication.
+ * Note that will this is currently the same as the dataset-integrations/update-service.ts;
+ * however, its purpose is not quite the same, so keeping it separate for now despite the duplication.
  */
 export class CreateService extends BaseService {
   constructor(
