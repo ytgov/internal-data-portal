01560a35d5c19e15ebd63832fb0a527f23e042b7
Marlen Brunner <klondikemarlen@gmail.com>
2024-05-17 15:09:48 -0700

    :sparkles: Add DatasetEntryPreviewsTable component concept.
    Now admins can switch between preview and non-preview mode in the dataset visualize/manage page.
    Preview will show a limited view of the dataset entries, while non-preview will show all entries.

---
+++ web/src/components/dataset-entry-previews/DatasetEntryPreviewsTable.vue
@@ -0,0 +1,22 @@
+<template>TODO: implement DatasetEntryPreviewsTable</template>
+
+<script lang="ts" setup>
+defineProps({
+  datasetId: {
+    type: Number,
+    required: true,
+  },
+  visualizationControlId: {
+    type: Number,
+    required: true,
+  },
+})
+
+function refresh() {
+  console.log('TODO: implement refresh')
+}
+
+defineExpose({
+  refresh,
+})
+</script>
--- web/src/pages/DatasetVisualizeManagePage.vue
+++ web/src/pages/DatasetVisualizeManagePage.vue
@@ -10,9 +10,24 @@
   />
 
   <v-spacer class="mt-6" />
+  <div class="d-flex justify-end">
+    <v-switch
+      v-model="showPreview"
+      :label="`Preview: ${showPreview ? 'On' : 'Off'}`"
+      color="primary"
+      inset
+    />
+  </div>
+  <v-spacer class="mt-6" />
   <v-skeleton-loader
     v-if="isNil(dataset)"
     type="table"
+  />
+  <DatasetEntryPreviewsTable
+    v-else-if="showPreview"
+    ref="datasetEntryPreviewsTable"
+    :dataset-id="dataset.id"
+    :visualization-control-id="dataset.visualizationControl.id"
   />
   <DatasetEntriesTable
     v-else
@@ -23,12 +38,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, toRefs } from "vue"
+import { ref, toRefs, watch } from "vue"
+import { useRoute, useRouter } from "vue-router"
 import { isNil } from "lodash"
 
 import { useBreadcrumbs } from "@/use/use-breadcrumbs"
 import { useDataset } from "@/use/use-dataset"
 
+import DatasetEntryPreviewsTable from "@/components/dataset-entry-previews/DatasetEntryPreviewsTable.vue"
 import DatasetEntriesTable from "@/components/dataset-entries/DatasetEntriesTable.vue"
 import VisualizePropertiesFormCard from "@/components/visualization-controls/VisualizePropertiesFormCard.vue"
 
@@ -42,15 +59,41 @@ const props = defineProps({
 const { slug } = toRefs(props)
 const { dataset } = useDataset(slug)
 
+const route = useRoute()
+const showPreview = ref(route.query.showPreview !== "false")
+
+const datasetEntryPreviewsTable = ref<InstanceType<typeof DatasetEntryPreviewsTable> | null>(null)
 const datasetEntriesTable = ref<InstanceType<typeof DatasetEntriesTable> | null>(null)
 
 function refreshTable() {
+  if (showPreview.value) {
+    if (isNil(datasetEntryPreviewsTable.value)) {
+      return
+    }
+
+    datasetEntryPreviewsTable.value.refresh()
+    return
+  }
+
   if (isNil(datasetEntriesTable.value)) {
     return
   }
 
   datasetEntriesTable.value.refresh()
 }
+
+const router = useRouter()
+
+watch(
+  () => showPreview.value,
+  (value) => {
+    if (value === false) {
+      router.push({ query: { showPreview: "false" } })
+    } else {
+      router.push({ query: { showPreview: undefined } })
+    }
+  }
+)
 
 const { setBreadcrumbs } = useBreadcrumbs()
 
