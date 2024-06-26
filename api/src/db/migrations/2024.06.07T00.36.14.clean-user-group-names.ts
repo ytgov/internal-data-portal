f39d47595e1cb6d380b9271226faf11a9fbe0030
Caleb Burke <calebburke91@gmail.com>
2024-06-26 14:55:18 -0700

    Raw sql query fix

--- api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
+++ api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
@@ -1,12 +1,9 @@
 import type { Migration } from "@/db/umzug"
 import { UserGroup } from "@/models"
 
-export const up: Migration = async () => {
-  await UserGroup.findEach(async (userGroup) => {
-    await userGroup.update({
-      name: userGroup.name.trim().replace(/\s+/g, " "),
-    })
-  })
+export const up: Migration = async ({ context: queryInterface }) => {
+  // https://stackoverflow.com/questions/6940646/mysql-how-to-remove-double-or-more-spaces-from-a-string
+  await queryInterface.sequelize.query(`UPDATE user_groups SET name = REPLACE(REPLACE(REPLACE(TRIM(name), ' ', '<>'), '><', ''), '<>', ' ');`);
 }
 
 export const down: Migration = async () => {
