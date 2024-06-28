c6818dd76decde6443badcaba2ff3faf818ba0e7
Caleb Burke <calebburke91@gmail.com>
2024-06-28 14:03:52 -0700

    Fix for the fix

--- api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
+++ api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
@@ -1,7 +1,11 @@
 import type { Migration } from "@/db/umzug"
+import { QueryTypes } from "sequelize";
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  const [rows]: any[] = await queryInterface.sequelize.query('SELECT name from user_groups');
+  const [rows] = await queryInterface.sequelize.query<{ id: number, name: string }[]>('SELECT name from user_groups', {
+    type: QueryTypes.SELECT,
+  });
+
   for (const row of rows) {
     const trimmedName = row.name.trim().replace(/\s+/g, " ")
     await queryInterface.sequelize.query('UPDATE user_groups SET name = ? WHERE id = ?', {
