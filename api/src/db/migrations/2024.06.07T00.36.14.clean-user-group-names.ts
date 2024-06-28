057f377560b9d700339422eb3670b09e0ba95bf4
Caleb Burke <calebburke91@gmail.com>
2024-06-28 13:27:59 -0700

    Fix for PR review

--- api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
+++ api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
@@ -1,16 +1,13 @@
 import type { Migration } from "@/db/umzug"
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.sequelize.transaction(async (transaction) => {
-    const [rows]: any[] = await queryInterface.sequelize.query('SELECT name from user_groups', { transaction });
-    for (const row of rows) {
-      const trimmedName = row.name.trim().replace(/\s+/g, " ")
-      await queryInterface.sequelize.query('UPDATE user_groups SET name = ? WHERE id = ?', {
-        replacements: [trimmedName, row.id],
-        transaction
-      });
-    }
-  });
+  const [rows]: any[] = await queryInterface.sequelize.query('SELECT name from user_groups');
+  for (const row of rows) {
+    const trimmedName = row.name.trim().replace(/\s+/g, " ")
+    await queryInterface.sequelize.query('UPDATE user_groups SET name = ? WHERE id = ?', {
+      replacements: [trimmedName, row.id]
+    });
+  }
 }
 
 export const down: Migration = async () => {
