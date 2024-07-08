cdbd1bf3f3355ae03dbeea9f65f7504f146bf5e3
Caleb Burke <calebburke91@gmail.com>
2024-07-08 12:08:53 -0700

    Fixed type issue

--- api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
+++ api/src/db/migrations/2024.06.07T00.36.14.clean-user-group-names.ts
@@ -1,19 +1,25 @@
-import type { Migration } from "@/db/umzug"
+import type { Migration } from "@/db/umzug";
 import { QueryTypes } from "sequelize";
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  const [rows] = await queryInterface.sequelize.query<{ id: number, name: string }[]>('SELECT name from user_groups', {
-    type: QueryTypes.SELECT,
-  });
-
+  const rows: { id: number, name: string }[] = await queryInterface.sequelize.query(
+    'SELECT id, name FROM user_groups',
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  
   for (const row of rows) {
-    const trimmedName = row.name.trim().replace(/\s+/g, " ")
-    await queryInterface.sequelize.query('UPDATE user_groups SET name = :trimmedName WHERE id = :userId', {
-      replacements: { trimmedName, userId: row.id }
-    });
+    const trimmedName = row.name.trim().replace(/\s+/g, " ");
+    await queryInterface.sequelize.query(
+      'UPDATE user_groups SET name = :trimmedName WHERE id = :userId',
+      {
+        replacements: { trimmedName, userId: row.id },
+      }
+    );
   }
-}
+};
 
 export const down: Migration = async () => {
   // no-op
-}
+};
